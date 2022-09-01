a417543be91b8aba9b81a39fbd13ee69cb3092df
romeu <passadore.neto@outlook.com>
2022-09-01 17:28:27 -0300

    novoGit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import ListaUsuario from './components/Usuario/ListaUsuario';
+import CadUsuario from './components/Usuario/CadUsuario';
 
 function App() {
 
@@ -14,7 +15,9 @@ function App() {
     tipo:'',
   }
 
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
   const [usuarios, setUsuario] = useState([])
+  const [objUsuario, setObjUsuario] = useState(usuario)
 
   useEffect(()=>{
 
@@ -24,9 +27,41 @@ function App() {
 
   },[]);
 
+  const aoDigitar = (e) => {
+
+    setObjUsuario({...objUsuario, [e.target.name]: e.target.value})
+    console.log(e)
+  
+  }
+
+  const cadastrarUsuario = () =>{
+
+    fetch('http://localhost:8080/api/usuario',{
+
+    method:'post',
+    body:JSON.stringify(objUsuario),
+    headers:{
+      'Content-type': 'application/json',
+      'Accept':'application/json'
+    }
+    }).then(retorno => retorno.json())
+    .then(retorno_convertido=>{
+
+      limparForm();
+    })
+    
+  }
+
+  const limparForm = () =>{
+
+    setObjUsuario(usuario)
+  }
+
+
   return (
     <div>
       <p>{JSON.stringify(usuarios)}</p>
+      <CadUsuario objeto={objUsuario} cadastrar={cadastrarUsuario} eventoTeclado={aoDigitar} botao={btnCadastrar}/>
       <ListaUsuario vetor={usuarios} />
     </div>
   );
