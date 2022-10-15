d88a3a55e4f24dc3463f05806ce201b1f28acf18
luzcaG <lucasfrr.23.email.com@gmail.com>
2022-10-15 15:51:41 -0300

    login tela pronto

--- src/Rotas.js
+++ src/Rotas.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import CadUsuario from './components/Usuario/CadUsuario';
 import CadTurma from "./components/Turma/PageTurma";
 import Login from "./components/login/login";
+import RedefinirSenha from "./components/login/redefinirSenha";
 
 function Rotas(){
 
@@ -10,6 +11,8 @@ function Rotas(){
             <Route path='/' element={<Login/>}/>
             <Route path='/usuarios' element={<CadUsuario/>}/>
             <Route path="/turmas" element={<CadTurma/>}/>
+            <Route path="/redefinirSenha" element={<RedefinirSenha/>}/>
+            
         </Routes>
     )
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -6,22 +6,31 @@ function Login() {
         <div className="divLogin">
             <header style={{color:"white"}}>
                 <img src={require("../login/LogoSenaiOriginal.png")}  className="logo"/>
+                <a href="/redefinirSenha" className="afolder">Folders</a>
             </header>
+            <div className="divs">
             <div className="div1">
                 
-                 <form className="formLogin">
-                    <h1>Login</h1>
-                    <div className="divInput">
-                    <input placeholder="NIF" className="inNif inputLogin"/>
-                    <input placeholder="Senha" className="insenha inputLogin"/>
-                    <button>Entrar</button>
-                    </div>
-                    
-                 </form>
+                <form className="formLogin">
+                   <h1 className="title">Login</h1>
+                   <div className="divInput">
+                   <input placeholder="NIF" className="inNif inputLogin" maxLength="9" minLength="7" required/>
+                   <input placeholder="Senha" className="insenha inputLogin" type={"password"} required/>
+                   <a href="/redefinirSenha" className="aEscSennha">Esqueceu a senha?</a>
+                   <div className="divBtn">
+                       <button className="btnEntrar">
+                               <p className="pEntrar">Entrar</p>
+                       </button>
+                   </div>
+               </div>
+                   
+                </form>
+           </div>
+           <div className="div2">
+                <img src={require("./imgPc.png")} className="imgDiv2"/>
+           </div>
             </div>
-            <div className="div2">
-                 
-            </div>
+            
         
             
         </div>
