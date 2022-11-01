1c9c82cebaac4b642b206abe0c6556edb03deb2f
romeu <passadore.neto@outlook.com>
2022-11-01 17:42:41 -0300

    redefinir senha

--- src/components/login/redefinirSenha.js
+++ src/components/login/redefinirSenha.js
@@ -1,41 +1,126 @@
 import React from "react";
-import "./RdfSenha.css"
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import "./RdfSenha.css";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const msgErroRedefinir = () => {
+  toast.error("E-mail não encontrada", {
+    position: "top-center",
+
+    autoClose: 1500,
+
+    hideProgressBar: false,
+
+    closeOnClick: true,
+
+    pauseOnHover: true,
+
+    theme: "colored",
+
+    // faz com que seja possivel arrastar
+
+    draggable: true,
+
+    progress: undefined,
+  });
+};
+const msgsussecoRedefinir = () => {
+    toast.success("Verifique o E-mail ", {
+      position: "top-center",
+  
+      autoClose: 1500,
+  
+      hideProgressBar: false,
+  
+      closeOnClick: true,
+  
+      pauseOnHover: true,
+  
+      theme: "colored",
+  
+      // faz com que seja possivel arrastar
+  
+      draggable: true,
+  
+      progress: undefined,
+    });
+  };
+
+const redefinirsenha = async (event) => {
+  /* tiras as características de evento evitando Recarregar  a pagina */
+  event.preventDefault();
+  /* pegar todos  os valores do evento */
+  const formData = new FormData(event.target);
+  /*formata em um objeto em  json */
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  let result = await fetch(`http://localhost:8080/api/usuario/redefinirSenha`, {
+    method: "post",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  if(result.status === 406){
+    msgErroRedefinir();
+
+
+  }else if(result){
+    msgsussecoRedefinir();
+
+  }
+};
 
 function RedefinirSenha() {
-    return(
-        <div className="divLogin">
-            <header style={{color:"white"}}>
-                <img src={require("../login/LogoSenaiOriginal.png")}  className="logo"/>
-            </header>
-            <div className="divs">
-            <div className="div1">
+  return (
+    <div className="divLogin">
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <header style={{ color: "white" }}>
+        <img src={require("../login/LogoSenaiOriginal.png")} className="logo" />
+      </header>
+      <div className="divs">
+        <div className="div1">
+          <form className="formRDFS" onSubmit={redefinirsenha}>
+            <a href="/login" className="aVoltar">
+              <button className="btnVoltar" type={"button"}>
+                <ArrowBackIcon className="arrow" fontSize="large" />
+              </button>
+            </a>
 
-                <form className="formRDFS">
-                    <a href="/login" className="aVoltar"><button className="btnVoltar" type={"button"}><ArrowBackIcon className="arrow" fontSize="large"/></button></a>
+            <h1 className="title">Redefinir Senha</h1>
+            <div className="divInput">
+              <input
+                placeholder="Email"
+                name="email"
+                className="inEmail inputLogin"
+                type={"email"}
+              />
 
-                   <h1 className="title">Redefinir Senha</h1>
-                   <div className="divInput">
-                   <input placeholder="Email" className="inEmail inputLogin" type={"email"}  />
-                  
-                   <div className="divBtn">
-                       <button className="btnRedefinir">
-                               <p className="pRedefinir">Redefinir</p>
-                       </button>
-                   </div>
-               </div>
-                   
-                </form>
-           </div>
-           <div className="div2">
-                <img src={require("./imgPc.png")} className="imgDiv2"/>
-           </div>
+              <div className="divBtn">
+                <button className="btnRedefinir" type="submit">
+                  <p className="pRedefinir">Redefinir</p>
+                </button>
+              </div>
             </div>
-            
-        
-            
+          </form>
         </div>
-    )
+        <div className="div2">
+          <img src={require("./imgPc.png")} className="imgDiv2" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RedefinirSenha;
