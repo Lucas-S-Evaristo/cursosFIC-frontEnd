fcb41816d5c2b030b5190b9d7d36baaab0bfafa8
Luzca gg <luzcagg@email.com>
2022-11-08 17:44:05 -0300

    home feito - 45%

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,37 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
+import ClassIcon from '@mui/icons-material/Class';
+import PeopleIcon from '@mui/icons-material/People';
 
 import "./hm.css";
 function Home() {
+  // variavel que tem acesso a um array com todos os cursos
+  const [cursos, setCursos] = useState([]);
+  // variavel que tem acesso a um array com todas as turmas
+  const [turmas, setTurma] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/curso")
+      .then((resp) => resp.json())
+      .then((retorno_convertido) => setCursos(retorno_convertido)); //lista de cursos
+  }, []);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/turma")
+      .then((resp) => resp.json())
+      .then((retorno_convertido) => setTurma(retorno_convertido)); //lista de turmas
+  }, []);
+
   return (
     <div className="divHome">
       <header className="headHome">
-        <img
-          src={require("./logoSenaiOrigin.png")}
-          className="logoHome"
-        />
-        <button
-        className="btLoginH"
-        >LOGIN</button>
+        <img src={require("./logoSenaiOrigin.png")} className="logoHome" />
+        <button className="btLoginH">LOGIN</button>
       </header>
       <div className="divEstatisticas">
         <section className="secEst">
-             Cursos
-            
-            <h1 className="subtitleSec">
-              Totais cursos disponiveis
-            </h1>
-            <p className="pEST">20</p>
+          <ClassIcon/>
+          <h1 className="subtitleSec">Totais cursos disponiveis</h1>
+          <p className="pEST" style={{ position: "relative", left: "30.2vh" }}>
+            {cursos.length}
+          </p>
         </section>
         <section className="secEst">
-            Turmas
-            <h1 className="subtitleSec" >
-              Totais turmas existentes
-            </h1>
-            <p className="pEST">20</p>
-        </section >
+          <PeopleIcon/>
+          <h1 className="subtitleSec">Totais turmas existentes</h1>
+          <p className="pEST">{turmas.length}</p>
+        </section>
       </div>
-      <p style={{position:"absolute",top:"10%",left:"50%", backgroundColor:"red"}}>.</p>
+     
+      <div className="divEstatisticas divEst">
+        <section className="secEst sec2">
+          <h1 className="subtitleSec ">Turmas Abertas</h1>
+          <p>20</p>
+        </section>
+        <section className="secEst sec2">
+          <h1 className="subtitleSec">Turmas Abertas</h1>
+          <p>20</p>
+        </section>
+        <section className="secEst sec2">
+          <h1 className="subtitleSec">Turmas Abertas</h1>
+          <p>20</p>
+        </section>
+
+        <section className="secEst sec2">
+          <h1 className="subtitleSec ">Turmas Abertas</h1>
+          <p>20</p>
+        </section>
+        <section className="secEst sec2">
+          <h1 className="subtitleSec">Turmas Abertas</h1>
+          <p>20</p>
+        </section>
+        <section className="secEst sec2">
+          <h1 className="subtitleSec">Turmas Abertas</h1>
+          <p>20</p>
+        </section>
+
+        <section className="secEst sec2">
+          <h1 className="subtitleSec ">Turmas Abertas</h1>
+          <p>20</p>
+        </section>
+        <section className="secEst sec2">
+          <h1 className="subtitleSec">Turmas Abertas</h1>
+          <p>20</p>
+        </section>
+        <section className="secEst sec2">
+          <h1 className="subtitleSec">Turmas Abertas</h1>
+          <p>20</p>
+        </section>
+
+        
+ 
+      </div>
     </div>
   );
 }
